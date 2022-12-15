fb70e3a6f6b87949d01d201b32730ca76f0b2f8c
Your Name <you@example.com>
2022-12-15 13:05:12 +0530

    new

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const name = 'Aniket';
+const img1 = "https://picsum.photos/600/500"
+const img2 = "https://picsum.photos/200/500"
+const img4 = "https://picsum.photos/300/500"
+const img5 = "https://picsum.photos/300/500"
+const img6 = "https://picsum.photos/300/500"
+const linkss = "https://www.linkedin.com/mynetwork/"
+
+ReactDOM.render(
+  <>
+    <h1 contentEditable="true"> My name is {name} </h1>
+
+    <img src={img1}  />
+    <img src={img1}  />
+    <a href={linkss} >
+    <img src={img1}  />
+</a>
+  </>
+  , document.getElementById('root')
+);
