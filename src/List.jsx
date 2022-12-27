df60c642492da8a4fce6c734c381870724a6ee5c
Your Name <you@example.com>
2022-12-27 21:53:58 +0530

    upd

---
+++ src/List.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function List(){
+    return(
+        <ol>
+            <li>A</li>
+            <li>B</li>
+            <li>C</li>
+            <li>D</li>
+        </ol>
+    )
+}
+export default List;
--- src/index.js
+++ src/index.js
@@ -1,21 +1,54 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+
+// const name = 'Aniket';
+// const img1 = "https://picsum.photos/600/500"
+// const img2 = "https://picsum.photos/200/500"
+
+// const linkss = "https://www.linkedin.com/mynetwork/"
+
+// ReactDOM.render(
+//   <>
+//     <h1 contentEditable="true"> My name is {name} </h1>
+
+//     <img src={img1}  />
+//     <img src={img1}  />
+//     <a href={linkss} >
+//     <img src={img1}  />
+// </a>
+//   </>
+//   , document.getElementById('root')
+// );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-const name = 'Aniket';
-const img1 = "https://picsum.photos/600/500"
-const img2 = "https://picsum.photos/200/500"
-
-const linkss = "https://www.linkedin.com/mynetwork/"
+import Heading from './Heading';
+import Paragraph from './Paragraph';
+import List from './List';
 
 ReactDOM.render(
   <>
-    <h1 contentEditable="true"> My name is {name} </h1>
-
-    <img src={img1}  />
-    <img src={img1}  />
-    <a href={linkss} >
-    <img src={img1}  />
-</a>
+    <Heading />
+    <Paragraph/>
+    <List />
   </>
   , document.getElementById('root')
 );
